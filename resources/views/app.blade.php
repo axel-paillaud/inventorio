efcd34a43f608e62b6390c4cddb50fb6bdda3235
axel-paillaud <contact@axelweb.fr>
2023-09-18 08:20:43 +0200

    delete welcome.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta description="test">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Vancraft') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
