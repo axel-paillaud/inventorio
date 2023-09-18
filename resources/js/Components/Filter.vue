ee5cd05df635de9c0b0beba11fe0486be994d9d9
axel-paillaud <contact@axelweb.fr>
2023-09-18 16:46:33 +0200

    add props name to Filter component

--- resources/js/Components/Filter.vue
+++ resources/js/Components/Filter.vue
@@ -1,6 +1,12 @@
 <script setup>
+defineProps({
+    name: String,
+    color: String
+})
 </script>
 
 <template>
-    <p>I'm a filter</p>
+    <button class="px-3 py-2 border rounded hover:bg-gray-100">
+        {{ name }}
+    </button>
 </template>
--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -7,7 +7,14 @@ import Filter from '@/Components/Filter.vue';
     <footer class="bg-white border-t border-gray-100">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between items-center h-16">
-                <Filter />
+                <!-- Filter container -->
+                <div class="flex gap-4">
+                    <Filter name="Tout"/>
+                    <Filter name="Partie cellules"/>
+                    <Filter name="Consommables"/>
+                    <Filter name="Outils"/>
+                    <Filter name="Pièces détachées"/>
+                </div>
                 <CreateFilter />
             </div>
         </div>
