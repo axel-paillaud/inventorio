6e89a76ebbac494197182b617b7b1c515573dc9c
axel-paillaud <contact@axelweb.fr>
2023-09-18 12:18:56 +0200

    HTML for Date component OK

--- resources/js/Components/Date.vue
+++ resources/js/Components/Date.vue
@@ -1,8 +1,32 @@
 <script setup>
+import { ChevronLeft } from 'lucide-vue-next';
+import { ChevronRight } from 'lucide-vue-next';
+import { ChevronDown } from 'lucide-vue-next';
+import Dropdown from '@/Components/Dropdown.vue';
 </script>
 
 <template>
-    <div>
+    <div class='flex'>
         <span>2023</span>
+        <button>
+            <ChevronLeft />
+        </button>
+        <button>
+            <ChevronRight />
+        </button>
+        <Dropdown>
+            <template #trigger>
+                <button>
+                    Année
+                    <ChevronDown />
+                </button>
+            </template>
+            <template #content>
+                <button>Jour</button>
+                <button>Mois</button>
+                <button>Année</button>
+                <button>Depuis toujours</button>
+            </template>
+        </Dropdown>
     </div>
 </template>
