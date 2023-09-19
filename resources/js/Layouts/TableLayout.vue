e7562cc4a38d2cdfcbd3047d2be05ba9f4a15a8e
axel-paillaud <contact@axelweb.fr>
2023-09-19 16:03:47 +0200

    CSS container for Table Ok

--- resources/js/Layouts/TableLayout.vue
+++ resources/js/Layouts/TableLayout.vue
@@ -2,32 +2,41 @@
 </script>
 
 <template>
-    <table
-        class="bg-white overflow-hidden shadow-md sm:rounded-lg w-full
-        border border-separate border-spacing-0"
-    >
-        <caption class="text-left">Pièces détachées</caption>
-        <thead class="text-left bg-lime-100">
-            <tr>
-                <th class="thead">Date</th>
-                <th class="thead">Nom</th>
-                <th class="thead">État</th>
-                <th class="thead">Quantité</th>
-                <th class="thead">Prix Unitaire</th>
-                <th class="thead">Total</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="n in 20">
-                <td class="td td-first" >10/08/2023</td>
-                <td class="td">Lame de ressort</td>
-                <td class="td">Occasion</td>
-                <td class="td">2</td>
-                <td class="td">150</td>
-                <td class="td td-last">300</td>
-            </tr>
-        </tbody>
-    </table>
+    <p class="px-6 py-1 text-lime-700">Pièces détachées</p>
+    <div class="max-h-96 border overflow-auto shadow-md rounded-lg relative">
+        <table
+            class="bg-white sm:rounded-lg w-full"
+        >
+            <thead class="text-left bg-lime-100 sticky top-0">
+                <tr>
+                    <th class="thead">Date</th>
+                    <th class="thead">Nom</th>
+                    <th class="thead">État</th>
+                    <th class="thead">Quantité</th>
+                    <th class="thead">Prix Unitaire</th>
+                    <th class="thead">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="n in 20">
+                    <td class="td td-first" >10/08/2023</td>
+                    <td class="td">Lame de ressort</td>
+                    <td class="td">Occasion</td>
+                    <td class="td">2</td>
+                    <td class="td">150</td>
+                    <td class="td td-last">300</td>
+                </tr>
+                <tr>
+                    <td class="td td-last-row" colspan="1">Total</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="td td-last-row td-total" colspan="1">128 000</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <style scoped>
@@ -46,6 +55,7 @@
 
 .td:hover {
     border-left-color: #e5e7eb;
+    border-left-width: 2px;
     border-right-color: #e5e7eb;
     border-top-color: #f3f4f6;
     background-color: #f3f4f6;
@@ -59,5 +69,13 @@
 .td-last:hover {
     border-left-color: #e5e7eb;
     border-right-color: #f3f4f6;
+}
+
+.td-last-row {
+    border: none;
+}
+
+.td-total {
+    grid-column: -1;
 }
 </style>
