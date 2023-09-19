cc3ed2140a037c7b24f06438264557d1330a483a
axel-paillaud <contact@axelweb.fr>
2023-09-19 11:51:16 +0200

    HTML for Table OK

---
+++ resources/js/Layouts/TableLayout.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+    <table
+        class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-full"
+    >
+        <caption class="text-left">Pièces détachées</caption>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Nom</th>
+                <th>État</th>
+                <th>Quantité</th>
+                <th>Prix Unitaire</th>
+                <th>Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="n in 20">
+                <td>10/08/2023</td>
+                <td>Lame de ressort</td>
+                <td>Occasion</td>
+                <td>2</td>
+                <td>150</td>
+                <td>300</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
--- resources/js/Pages/Table/Dashboard.vue
+++ resources/js/Pages/Table/Dashboard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import TableLayout from '@/Layouts/TableLayout.vue';
 import Footer from '@/Components/Footer.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
@@ -10,9 +11,7 @@ import { Head } from '@inertiajs/vue3';
     <AuthenticatedLayout>
         <main class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
+                <TableLayout />
             </div>
         </main>
 
