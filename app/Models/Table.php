040c7b9962879f41d2e3edd4689fb9fbf30fca1c
axel-paillaud <contact@axelweb.fr>
2023-09-22 16:46:56 +0200

    create models and relations

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Relations\BelongsTo;
+use Illuminate\Database\Relations\hasMany;
 
 class Table extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function tableRows(): hasMany
+    {
+        return $this->hasMany(TableRow::class);
+    }
+
+    public function user(): belongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/TableRow.php
+++ app/Models/TableRow.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
 
 class TableRow extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function table(): belongsTo
+    {
+        return $this->belongsTo(Table::class);
+    }
 }
