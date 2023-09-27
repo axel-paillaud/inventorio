87e7e50fa4f5645c88ec4fc826adce1880d967c4
axel-paillaud <contact@axelweb.fr>
2023-09-27 09:37:06 +0200

    separate table row in TableRow component

---
+++ resources/js/Components/TableRow.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+</script>
+
+<template>
+    <tr>
+        <td class="td td-first">
+            <input
+                class="input-cell w-full focus:ring-0"
+                type="date"
+                value="2018-07-22"
+            >
+            <div class="fixed-cell">
+                2018-07-22
+            </div>
+        </td>
+        <td class="td">
+            <input
+                class="input-cell w-full focus:ring-0"
+                type="text"
+                value="Lame de ressort"
+            >
+            <div class="fixed-cell">
+                Lame de ressort
+            </div>
+        </td>
+        <td class="td">
+            <input
+                class="input-cell w-full focus:ring-0"
+                type="text"
+                value="Occasion"
+            >
+            <div class="fixed-cell">
+                Occasion
+            </div>
+        </td>
+        <td class="td">
+            <input
+                class="input-cell w-full focus:ring-0"
+                type="number"
+                value="2"
+            >
+            <div class="fixed-cell">
+                2
+            </div>
+        </td>
+        <td class="td">
+            <input
+                class="input-cell w-full focus:ring-0"
+                type="number"
+                value="150"
+            >
+            <div class="fixed-cell">
+                150
+            </div>
+        </td>
+        <td class="td">
+            <div class="fixed-cell">300</div>
+        </td>
+    </tr>
+</template>
--- resources/js/Layouts/TableLayout.vue
+++ resources/js/Layouts/TableLayout.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Plus } from 'lucide-vue-next';
+import TableRow from '@/Components/TableRow.vue';
 
 </script>
 
@@ -36,68 +37,14 @@ import { Plus } from 'lucide-vue-next';
                     </tr>
                 </tfoot>
                 <tbody>
-                    <tr v-for="n in 20">
-                        <td class="td td-first">
-                            <input
-                                class="input-cell w-full focus:ring-0"
-                                type="date"
-                                value="2018-07-22"
-                            >
-                            <div class="fixed-cell">
-                                2018-07-22
-                            </div>
-                        </td>
-                        <td class="td">
-                            <input
-                                class="input-cell w-full focus:ring-0"
-                                type="text"
-                                value="Lame de ressort"
-                            >
-                            <div class="fixed-cell">
-                                Lame de ressort
-                            </div>
-                        </td>
-                        <td class="td">
-                            <input
-                                class="input-cell w-full focus:ring-0"
-                                type="text"
-                                value="Occasion"
-                            >
-                            <div class="fixed-cell">
-                                Occasion
-                            </div>
-                        </td>
-                        <td class="td">
-                            <input
-                                class="input-cell w-full focus:ring-0"
-                                type="number"
-                                value="2"
-                            >
-                            <div class="fixed-cell">
-                                2
-                            </div>
-                        </td>
-                        <td class="td">
-                            <input
-                                class="input-cell w-full focus:ring-0"
-                                type="number"
-                                value="150"
-                            >
-                            <div class="fixed-cell">
-                                150
-                            </div>
-                        </td>
-                        <td class="td">
-                            <div class="fixed-cell">300</div>
-                        </td>
-                    </tr>
+                    <TableRow />
                 </tbody>
             </table>
         </div>
     </div>
 </template>
 
-<style scoped>
+<style>
 .thead {
     padding: 12px 24px;
     border-bottom: 1px solid #e5e7eb;
