945f3adc04c856e6a3ce59da7b0624148e6d64a7
axel-paillaud <contact@axelweb.fr>
2023-10-03 16:13:53 +0200

    delete old classic <style> for td

--- resources/js/Components/Row.vue
+++ resources/js/Components/Row.vue
@@ -26,57 +26,9 @@ const total = formatter.format(props.total);
         <StateCell :state="state" />
         <QuantityCell :quantity="quantity" />
         <PriceCell :price="price" />
-        <td class="td-total">
-            <div class="fixed-cell">{{ total }}</div>
+        <td class="p-0 border border-white border-b-gray-100">
+            <div class="py-3 px-6">{{ total }}</div>
         </td>
     </tr>
 </template>
 
-<style scoped>
-.td {
-    padding: 0;
-    border: 1px solid white;
-    border-bottom: 1px solid var(--gray-100);
-    transition: background-color 0.2s ease;
-    cursor: text;
-}
-
-.td:hover {
-    border-top: 1px solid var(--gray-100);
-    border-left: 1px solid var(--gray-200);
-    border-right: 1px solid var(--gray-200);
-    background-color: var(--gray-100);
-}
-
-.td-total {
-    padding: 0;
-    border: 1px solid white;
-    border-bottom: 1px solid var(--gray-100);
-}
-
-.input-cell {
-    padding: 12px 24px;
-    transition: all 0.2s ease;
-    transition: background-color 0.2s ease;
-}
-
-.input-cell:focus {
-    border-left-color: var(--gray-200);
-    border-right-color: var(--gray-200);
-    border-top-color: var(--gray-100);
-    background-color: var(--gray-100);
-}
-
-.input-cell:hover {
-    border-left-color: var(--gray-200);
-    border-right-color: var(--gray-200);
-    border-top-color: var(--gray-100);
-    background-color: var(--gray-100);
-}
-
-.fixed-cell {
-    padding: 12px 24px;
-    max-height: 145px;
-    overflow: hidden;
-}
-</style>
