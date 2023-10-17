e8641864f4ed86faa7ba9f0c016c254562e027ab
axel-paillaud <contact@axelweb.fr>
2023-10-17 12:12:29 +0200

    test for InputOverlay with teleport ok

--- tests/Unit/Vue/InputOverlay.test.js
+++ tests/Unit/Vue/InputOverlay.test.js
@@ -2,28 +2,76 @@ import { mount } from '@vue/test-utils';
 import InputOverlay from '@/Components/InputOverlay.vue';
 import DateCell from '@/Components/Cells/Date.vue';
 
-test('InputOverlay emit closeCell when we click on it', async () => {
-    const wrapper = mount(InputOverlay, {
-        props: {
-            isActive: true
-        }
+afterEach(() => {
+  // clean up
+  document.body.innerHTML = ''
+})
+
+describe('InputOverlay', () => {
+    test('InputOverlay emit closeCell when we click on it', async () => {
+        const wrapper = mount(InputOverlay, {
+            props: {
+                isActive: true
+            }
+        });
+
+        // Overlay is 'Teleport' to the body, so we can not use wrapper here
+        document.querySelector('[data-testid="overlay"]').click();
+
+        expect(wrapper.emitted()).toHaveProperty('closeCell');
     });
 
-    console.log(document.body.innerHTML);
-    wrapper.find('[data-testid="overlay"]').trigger('click');
+    test('InputOverlay emit closeCell when we press enter', async () => {
+        const wrapper = mount(InputOverlay, {
+            props: {
+                isActive: true
+            },
+            emits: ['closeCell'],
+        });
 
-    // expect(wrapper.emitted()).toHaveProperty('closeCell');
+        const event = new KeyboardEvent('keydown', {
+            key: 'Enter',
+            code: 'Enter',
+            keyCode: 13,
+            which: 13,
+
+        });
+
+        document.dispatchEvent(event);
+
+        expect(wrapper.emitted()).toHaveProperty('closeCell');
+    });
+
+    test('InputOverlay emit closeCell when we press escape', async () => {
+        const wrapper = mount(InputOverlay, {
+            props: {
+                isActive: true
+            },
+            emits: ['closeCell'],
+        });
+
+        const event = new KeyboardEvent('keydown', {
+            key: 'Escape',
+            code: 'Escape',
+            keyCode: 27,
+            which: 27,
+
+        });
+
+        document.dispatchEvent(event);
+
+        expect(wrapper.emitted()).toHaveProperty('closeCell');
+    });
+
+    test('Overlay exist when isActive prop is true', async () => {
+        // We only use props on this wrapper
+        const wrapper = mount(InputOverlay, {
+            props: {
+                isActive: true
+            }
+        });
+
+        expect(document.querySelector('[data-testid="overlay"]')).not.toBeNull();
+    });
 });
 
-// test('InputOverlay emit closeCell when we press enter', async () => {
-//        const wrapper = mount(InputOverlay, {
-//         props: {
-//             isActive: true
-//         },
-//         emits: ['closeCell'],
-//     });
-//
-//     // Doesn't work because we need to test a Composable. It's not the overlay
-//     // div that emit 'closeCell' when we press Enter
-//     //await wrapper.find('[data-testid="overlay"]').trigger('keydown', { keyCode: 13 });
-// });
