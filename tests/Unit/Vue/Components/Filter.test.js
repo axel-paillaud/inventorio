2faa221e7d2bd613e1bec32ac0c276d6afcbac1e
axel-paillaud <contact@axelweb.fr>
2023-10-20 11:24:13 +0200

    unit test for Filter component : isActive ok

--- tests/Unit/Vue/Components/Filter.test.js
+++ tests/Unit/Vue/Components/Filter.test.js
@@ -1,20 +1,45 @@
 import { mount } from '@vue/test-utils';
-import Filter from '@/Components/Filter';
+import Filter from '@/Components/Filter.vue';
 import colors from '@/Services/ColorService';
 
 describe('Filter', () => {
-    test('When filter is active, always set a bg color', () => {
+    test('When filter is active, always set a bg color', async () => {
+        const props = {
+            isActive: true,
+            color: 'green',
+            name: 'foo',
+
+        }
         const wrapper = mount(Filter, {
-            props: {
-                isActive: true,
-                color: 'green',
-                name: 'foo',
-            }
+            props: props
         });
 
-        const filterButton = wrapper.find('[data-testid="filterButton"]');
-        console.log(filterButton.html());
+        let filterButton = wrapper.find('[data-testid="filter-button"]');
+        expect(filterButton.classes()).toContain(colors[props.color].bg);
 
-        expect(true).toBe(true);
+        props.color = 'blue';
+        await wrapper.setProps(props);
+        filterButton = wrapper.find('[data-testid="filter-button"]');
+        expect(filterButton.classes()).toContain(colors[props.color].bg);
+    });
+
+    test('When filter is not active, bg color is not set', async () => {
+        const props = {
+            isActive: false,
+            color: 'green',
+            name: 'foo',
+
+        }
+        const wrapper = mount(Filter, {
+            props: props
+        });
+
+        let filterButton = wrapper.find('[data-testid="filter-button"]');
+        expect(filterButton.classes(colors[props.color].bg)).toBe(false);
+
+        props.color = 'blue';
+        await wrapper.setProps(props);
+        filterButton = wrapper.find('[data-testid="filter-button"]');
+        expect(filterButton.classes(colors[props.color].bg)).toBe(false);
     });
 });
