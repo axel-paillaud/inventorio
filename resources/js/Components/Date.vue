26dccee9d35c579e52f7aacea377255a72905529
axel-paillaud <contact@axelweb.fr>
2023-11-01 19:35:06 +0100

    switch year wip

--- resources/js/Components/Date.vue
+++ resources/js/Components/Date.vue
@@ -1,22 +1,48 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { Link } from '@inertiajs/vue3';
 import { ChevronLeft } from 'lucide-vue-next';
 import { ChevronRight } from 'lucide-vue-next';
 import { ChevronDown } from 'lucide-vue-next';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
+
+const year = ref(0);
+const month = ref(0);
+const day = ref(0);
+
+onMounted(() => {
+    const currentDate = new Date();
+    year.value = currentDate.getFullYear();
+    month.value = currentDate.getMonth();
+    day.value = currentDate.getDate();
+});
+
+const callback = () => {
+    year.value -= 1;
+}
 </script>
 
 <template>
     <div class="flex items-center text-sm gap-4">
         <!-- Date and arrow container -->
         <div class="flex items-center gap-4">
-            <span>2023</span>
+            <span>{{ year }}</span>
             <!-- Arrow container -->
             <div class="flex items-center gap-1.5">
-                <button class="rounded-full hover:bg-gray-100 p-1.5">
+                <!-- slot ici ne change rien ? comment ne pas reload ce component ? -->
+                <Link
+                    class="rounded-full hover:bg-gray-100 p-1.5"
+                    @click="callback"
+                    :href="route('date.year', year)"
+                >
                     <ChevronLeft :size="20"/>
-                </button>
-                <button class="rounded-full hover:bg-gray-100 p-1.5">
+                    <slot />
+                </Link>
+                <button
+                    class="rounded-full hover:bg-gray-100 p-1.5"
+                    @click="year += 1"
+                >
                     <ChevronRight :size="20"/>
                 </button>
             </div>
@@ -35,7 +61,7 @@ import DropdownLink from '@/Components/DropdownLink.vue';
                 <div class="flex flex-col">
                     <DropdownLink :href="route('date.day', { year: 2023, month: 9, day: 27})"> Jour </DropdownLink>
                     <DropdownLink :href="route('date.month', { year: 2023, month: 9 })"> Mois </DropdownLink>
-                    <DropdownLink :href="route('date.year', '2023')"> Année </DropdownLink>
+                    <DropdownLink :href="route('date.year', year)"> Année </DropdownLink>
                     <DropdownLink :href="route('profile.edit')"> Depuis toujours</DropdownLink>
                 </div>
             </template>
