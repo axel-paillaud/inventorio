94fd96f2a734439a38f630f49d9b3fb215270b31
axel-paillaud <contact@axelweb.fr>
2023-12-05 09:39:00 +0100

    remove parseUrl composable, we don't parse url to get date anymore

--- resources/js/Composables/parseUrl.js
+++
@@ -1,7 +0,0 @@
-export function checkDateTypeInUrl(url) {
-    if (/\/year\//.test(url)) return 'year';
-    else if (/\/month\//.test(url)) return 'month';
-    else if (/\/day\//.test(url)) return 'day';
-    return 'always';
-}
-
