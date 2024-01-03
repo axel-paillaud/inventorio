bb033827e9884cfc6fb95f5428c0faa0cecdf5bf
axel-paillaud <contact@axelweb.fr>
2024-01-03 11:56:23 +0100

    recursive issue with watch on Cells/date.vue file, wip

--- resources/js/Components/Cells/Date.vue
+++ resources/js/Components/Cells/Date.vue
@@ -2,6 +2,7 @@
 import { ref, computed, watch } from 'vue';
 import InputOverlay from '@/Components/InputOverlay.vue';
 import { isDateValid } from '@/Composables/validators/date';
+import { router } from '@inertiajs/vue3';
 
 const props = defineProps({
     date: String,
@@ -11,17 +12,22 @@ const props = defineProps({
 const isActive = ref(false);
 const date = ref(props.date);
 
-watch(date, (newDate, oldDate) => {
-    if (isDateValid(date)) {
-        date.value = newDate;
-    } else {
-        date.value = oldDate;
-    }
-});
+// watch(date, (newDate, oldDate) => {
+//     if (isDateValid(date)) {
+//         date.value = newDate;
+//     } else {
+//         date.value = oldDate;
+//     }
+// });
 
 const formattedDate = computed(() => {
+    console.log(date.value);
     return new Date(date.value).toLocaleDateString();
 });
+
+function submitCellData() {
+    console.log("hello, franck!");
+}
 
 </script>
 
@@ -33,13 +39,15 @@ const formattedDate = computed(() => {
         hover:border-r-gray-200 hover:border-l-gray-200"
     >
         <InputOverlay :isActive="isActive"  @closeCell="isActive = false" />
-        <input
-            v-show="isActive"
-            class="py-3 px-6 focus:ring-0 transition-colors absolute inset-0 w-48
-            z-60 bg-gray-50 border-gray-500"
-            type="date"
-            v-model="date"
-        >
+        <form @submit.prevent="submitCellData">
+            <input
+                v-show="isActive"
+                class="py-3 px-6 focus:ring-0 transition-colors absolute inset-0 w-48
+                z-60 bg-gray-50 border-gray-500"
+                type="date"
+                v-model="date"
+            >
+        </form>
         <div class="py-3 px-6" :class="{ invisible: isActive }">
             {{ formattedDate }}
         </div>
