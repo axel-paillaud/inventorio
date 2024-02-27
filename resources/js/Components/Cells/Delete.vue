f8f68d9e75e74ec0af611c637275614a6138417f
axel-paillaud <contact@axelweb.fr>
2024-02-27 09:15:55 +0100

    delete useless code after set height on delete cells

--- resources/js/Components/Cells/Delete.vue
+++ resources/js/Components/Cells/Delete.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue';
 import { useForm } from '@inertiajs/vue3';
 import { Trash } from 'lucide-vue-next';
 
@@ -7,20 +6,12 @@ const props = defineProps({
     rowId: Number,
 });
 
-const cell = ref(null);
-const container = ref(null);
-
-onMounted(() => {
-    // const height = ref(cell.value.clientHeight);
-    // container.value.style.height = `${cell.value.clientHeight}px`;
-});
-
 </script>
 
 <template>
     <td ref="cell" class="group border-b border-b-gray-100 h-full"
     >
-        <div ref="container"
+        <div
             class="relative hover:bg-red-100 transition-all rounded-r-md flex
             right-8 group-hover:right-0 group-hover:bg-red-100 h-full"
         >
