22ff94c7d87f635e80e00193716a3725c4cc05ef
axel-paillaud <contact@axelweb.fr>
2024-02-29 11:35:28 +0100

    refacto QuantityController : use relationship instead of where clause

--- app/Http/Controllers/Inventorio/Cells/QuantityController.php
+++ app/Http/Controllers/Inventorio/Cells/QuantityController.php
@@ -3,23 +3,19 @@
 namespace App\Http\Controllers\Inventorio\Cells;
 
 use Illuminate\Http\Request;
-use App\Models\TableRow;
 use App\Http\Controllers\Controller;
 
 class QuantityController extends Controller
 {
     public function update(Request $request)
     {
-        $user = $request->user();
-
         $validated = $request->validate([
             'quantity' => ['required', 'integer'],
             'row_id' => ['required', 'integer', 'exists:table_rows,id'],
         ]);
 
-        TableRow::where([
-            ['user_id', $user->id],
-            ['id', $validated['row_id']],
-        ])->update(['quantity' => $validated['quantity']]);
+        $request->user()->rows()->where('id', $validated['row_id'])->update([
+            'quantity' => $validated['quantity']
+        ]);
     }
 }
