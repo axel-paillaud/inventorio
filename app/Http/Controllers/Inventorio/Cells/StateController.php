958ad59eb3900b94657d6097a760f34e21c19fd1
axel-paillaud <contact@axelweb.fr>
2024-02-29 11:36:47 +0100

    refacto StateController : use relationship instead of where c lause

--- app/Http/Controllers/Inventorio/Cells/StateController.php
+++ app/Http/Controllers/Inventorio/Cells/StateController.php
@@ -4,22 +4,18 @@ namespace App\Http\Controllers\Inventorio\Cells;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\TableRow;
 
 class StateController extends Controller
 {
     public function update(Request $request)
     {
-        $user = $request->user();
-
         $validated = $request->validate([
             'state' => ['required', 'in:Neuf,Occasion,A réparer'],
             'row_id' => ['required', 'integer', 'exists:table_rows,id'],
         ]);
 
-        TableRow::where([
-            ['user_id', $user->id],
-            ['id', $validated['row_id']],
-        ])->update(['state'=> $validated['state']]);
+        $request->user()->rows()->where('id', $validated['row_id'])->update([
+            'state'=> $validated['state']
+        ]);
     }
 }
