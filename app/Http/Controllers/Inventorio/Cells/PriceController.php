4845d8c85810b2ef2bd1c65afc7074cfb15c1d19
axel-paillaud <contact@axelweb.fr>
2024-02-29 11:33:51 +0100

    refacto PriceController : use relationship instead of where clause

--- app/Http/Controllers/Inventorio/Cells/PriceController.php
+++ app/Http/Controllers/Inventorio/Cells/PriceController.php
@@ -4,22 +4,18 @@ namespace App\Http\Controllers\Inventorio\Cells;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\TableRow;
 
 class PriceController extends Controller
 {
     public function update(Request $request)
     {
-        $user = $request->user();
-
         $validated = $request->validate([
             'price' => ['required', 'numeric', 'min:0'],
             'row_id' => ['required', 'integer', 'exists:table_rows,id'],
         ]);
 
-        TableRow::where([
-            ['user_id', $user->id],
-            ['id', $validated['row_id']],
-        ])->update(['price'=> $validated['price']]);
+        $request->user()->rows()->where('id', $validated['row_id'])->update([
+            'price'=> $validated['price']
+        ]);
     }
 }
