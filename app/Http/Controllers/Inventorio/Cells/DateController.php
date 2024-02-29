94c17de4d1a4d625bced7bd67eca0671595ede34
axel-paillaud <contact@axelweb.fr>
2024-02-29 11:16:22 +0100

    refacto DateController : use relationship instead of where clause

--- app/Http/Controllers/Inventorio/Cells/DateController.php
+++ app/Http/Controllers/Inventorio/Cells/DateController.php
@@ -10,17 +10,14 @@ class DateController extends Controller
 {
     public function update(Request $request)
     {
-        $user = $request->user();
-
         $validated = $request->validate([
             'date' => ['required', 'date_format:Y-m-d'],
             'row_id' => ['required', 'integer', 'exists:table_rows,id'],
         ]);
 
-        TableRow::where([
-            ['user_id', $user->id],
-            ['id', $validated['row_id']],
-        ])->update(['date' => $validated['date']]);
+        $request->user()->rows($validated['row_id'])->update([
+            'date' => $validated['date']
+        ]);
     }
 
 }
