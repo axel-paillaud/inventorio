ded5e5ce3cee4eda1cc0941da5cb373fc3fe00ed
axel-paillaud <contact@axelweb.fr>
2024-03-01 12:15:18 +0100

    import i18n in app.js, create resources/js/lang/messages.js translate file

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,14 @@ import { createPinia } from 'pinia';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createI18n } from 'vue-i18n';
+import messages from '@/lang/messages.js';
+
+const i18n = createI18n({
+    locale: 'fr',
+    fallbackLocale: 'en',
+    messages
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,6 +25,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(i18n)
             .use(pinia)
             .use(ZiggyVue, Ziggy)
             .mount(el);
---
+++ resources/js/lang/messages.js
@@ -0,0 +1,18 @@
+export default {
+    en: {
+        nav: {
+            language_full: 'English',
+        },
+        auth: {
+            logout: "Log Out",
+        }
+    },
+    fr: {
+        nav: {
+            language_full: 'French',
+        },
+        auth: {
+            logout: "Déconnexion",
+        }
+    }
+}
