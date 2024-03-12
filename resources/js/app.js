ec22beb1a28c159ca421d460efce6b18b835bdd1
axel-paillaud <contact@axelweb.fr>
2024-03-12 07:21:33 +0100

    uninstall pinia packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
-import { createPinia } from 'pinia';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
@@ -18,8 +17,6 @@ const i18n = createI18n({
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-const pinia = createPinia();
-
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
@@ -27,7 +24,6 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(i18n)
-            .use(pinia)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
