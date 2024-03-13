1756d105bc82904c0ba71c2eb722a5aa48fd480f
axel-paillaud <contact@axelweb.fr>
2024-03-13 14:07:51 +0100

    add register button on default login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -89,6 +89,14 @@ const submit = () => {
                     Log in
                 </PrimaryButton>
             </div>
+            <div class="flex justify-end mt-4">
+                <Link
+                    :href="route('register')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Need to register?
+                </Link>
+            </div>
         </form>
     </GuestLayout>
 </template>
