4888c5838aab7554e63d2eaa0506d9d652c88522
axel-paillaud <contact@axelweb.fr>
2024-03-13 15:05:00 +0100

    add header to GuestLayout component, with language selection

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,15 +1,25 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import LanguageSelection from '@/Components/LanguageSelection.vue';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                Home
-            </Link>
-        </div>
+    <header class="sticky top-0 z-20 h-full bg-white">
+        <div
+            class="mx-auto px-4 sm:px-8 lg:px-12 h-16 mx-auto px-4 sm:px-8 lg:px-12
+            justify-between flex items-center"
+        >
+            <div>
+                <Link href="/">
+                    Home
+                </Link>
+            </div>
 
+            <LanguageSelection />
+
+        </div>
+    </header>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
         >
